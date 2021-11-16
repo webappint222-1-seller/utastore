f2eea864e0b9f97859d65441eeb68582e2c746cb
Korawit Rittisit <korawit.oum@mail.kmutt.ac.th>
2021-11-16 16:42:22 +0700

    fix AccessControl

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.header('Access-Control-Allow-Credentials', true); 
+  // res.header('Access-Control-Allow-Credentials', true); 
   next();
 })
 app.use(cookieParser());
